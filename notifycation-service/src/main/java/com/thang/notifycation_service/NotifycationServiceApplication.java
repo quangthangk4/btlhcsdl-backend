98db00bbffee0dd6f693f43b3063bd1f4f862089
quangthangk4 <quangthangkhmt.k22@gmail.com>
2025-04-21 23:46:48 +0700

    add notifycation service

---
+++ notifycation-service/src/main/java/com/thang/notifycation_service/NotifycationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thang.notifycation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifycationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifycationServiceApplication.class, args);
+	}
+
+}
